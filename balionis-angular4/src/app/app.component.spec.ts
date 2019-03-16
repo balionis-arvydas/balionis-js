dd737494c06e191f46fea066999e94e3ede8b348
baliarvy <arvydas@balionis.com>
2019-03-16 09:12:54 +0000

    JIRA-0000 add default ng new generated

--- balionis-angular4/src/app/app.component.spec.ts
+++ balionis-angular4/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'balionis-angular3'`, () => {
+  it(`should have as title 'balionis-angular4'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('balionis-angular3');
+    expect(app.title).toEqual('balionis-angular4');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to balionis-angular3!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to balionis-angular4!');
   });
 });
